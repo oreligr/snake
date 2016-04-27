20c0ed4d7b1d8d1f3d68e7d132910753e0909749
oreligr <oreligr@rambler.ru>
2016-04-27 20:38:13 +0200

    firtstly

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Draw(2,3,'*');
+        Draw(3,4,'-');
+
+    }
+    static void Draw(int x, int y, char sym){
+        for(int i = 0; i<y;i++){
+            System.out.println();
+            for(int j=0;j<x;j++){
+                System.out.print(" ");
+            }
+        }
+        System.out.println(sym);
+       
     }
 }
